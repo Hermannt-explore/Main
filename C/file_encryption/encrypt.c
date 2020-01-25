7592d980cb5457fb2a9f6010e326c1f434e1d8ab
Hermannt-explore <hermannchedjou@yahoo.com>
2020-01-25 18:02:30 -0500

    final update

--- C/file_encryption/encrypt.c
+++ C/file_encryption/encrypt.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 void main(){
 int i;
@@ -7,21 +8,26 @@ char initkey[6];
 char key[6];
 char c;
 FILE *in, *enc;
-in=fopen("input.txt", "r");
-enc=fopen("encrypted.txt", "w");
-if(in == NULL ){exit(0);}
+char name[100];
+/*opening prompt*/
+printf("Please enter name of file to encrypt\n");
+fgets( name, 100, stdin);		//Read file name from user.
+name[strcspn(name, "\n")] = '\0'; 	//Discard characters after the new line.
+in=fopen( name, "r");			//Open file.
+if(in == NULL ){printf("file does not exist in this directory!"); exit(0);}
+enc=fopen("encrypted.txt", "w");	//Create output file.
 
 printf("PLease enter 5-character encryption key : ");
+/*Get key from user*/ 
 for(i=0; i<6; i++){
 	scanf("%c", &initkey[i]);
 	if(initkey[0] == '\n')
 	{	break;
 	}
 	if( (initkey[1] == '\n') |  (initkey[2] == '\n') |  (initkey[3] == '\n') |  (initkey[4] == '\n') )
-	{	printf("Encryption failed! Your key did not contain 5 characters.\n");exit(0);
-	}	
+	{printf("Encryption failed! Your key did not contain 5 characters.\n"); exit(0);}	
 }
-
+/*use key aaaaa if no key was entered.*/
 if(initkey[0] == '\n'){
 	for(i=0; i<5; i++){
 		key[i] = 'a';
@@ -35,6 +41,7 @@ else{
 	key[5]= '\n';
 }
 i=0;
+/*File encryption*/
 while( (c=fgetc(in)) != EOF){
 	if( i==5 ){
 	i=0;
@@ -45,7 +52,10 @@ while( (c=fgetc(in)) != EOF){
 }
 fclose(in);
 fclose(enc);
-printf("your file \"input.txt\" has been encrypted with your key ");
+/*Closing prompts*/
+printf("your file \"");
+printf("%s", name); 
+printf("\" has been encrypted with your key ");
 for(i=0; i<6; i++){
 	printf("%c", key[i]);
 }
